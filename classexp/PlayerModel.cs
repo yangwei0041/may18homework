dd128c59418463bedabc47eb7c12b5466909b2d3
weiyangoxy <wei_yang@oxy.com>
2022-05-19 13:19:24 -0500

    Add project files.

---
+++ classexp/PlayerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class PlayerModel : IPlayerModel
+    {
+        private readonly Guid _id = Guid.NewGuid();
+        public Guid Id { get { return _id; } }
+        public string Name { get; set; }
+        public delegate void PrintPlayerInfo(PlayerModel p);
+        public PlayerModel() { }
+        public PlayerModel(string name)
+        {
+            Name = name;
+        }
+        public void Print(PrintPlayerInfo printPlayer, PlayerModel Player)
+        {
+            printPlayer(player);
+        }
+    }
+}
---
+++ classexp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Delegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayerModel p1 = new PlayerModel();
+            p1.Name = "Daniel";
+            p1.Print(PrintPlayerName, p1);
+            p1.Print(PrintPlayerId, p1);
+
+            Console.ReadKey();
+
+        }
+
+        private static void PrintPlayerName(PlayerModel pl)
+        {
+            Console.WriteLine($"player name is: {pl.Name}");
+        }
+        private static void PrintPlayerId(PlayerModel pl)
+        {
+            Console.WriteLine($"player name is: {pl.Name}");
+            Console.WriteLine($"player id is: {pl.Id}");
+        }
+    }
+}
